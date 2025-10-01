4843e4193531e69ae5e783fba31a5e76225b5d2d
Luke Dixon <github@lukedixon.nl>
2025-10-01 12:04:50 +0200

    Refactor page structure by removing unused all_locations page and updating page selection logic in home.py

--- home.py
+++ home.py
@@ -11,13 +11,7 @@ def page2():
     sl.markdown("# Pagina 2 ")
 
 
-page_names_to_funcs = {
-    "Homepagina": main_page,
-    "Alle Locaties": page2
 
-}
-selected_page = sl.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
-page_names_to_funcs[selected_page]()
 
 
 
