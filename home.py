e175fab01226b67adc7176f30de2e6dce5687595
Luke Dixon <github@lukedixon.nl>
2025-10-01 13:48:06 +0200

    edited file name's

--- home.py
+++ home.py
@@ -1,14 +1,22 @@
-
 import streamlit as sl
 import pandas as pd
 
+elke_s_flight_1 = pd.read_excel('data/30sFlight_1.xlsx')
+elke_s_flight_2 = pd.read_excel('data/30sFlight_2.xlsx')
+elke_s_flight_3 = pd.read_excel('data/30sFlight_3.xlsx')
+elke_s_flight_4 = pd.read_excel('data/30sFlight_4.xlsx')
+elke_s_flight_5 = pd.read_excel('data/30sFlight_5.xlsx')
+elke_s_flight_6 = pd.read_excel('data/30sFlight_6.xlsx')
+
+
+print(elke_s_flight_1.info())
 
 def main_page():
     sl.markdown("# Homepagina ")
 
 
 def page2():
-    sl.markdown("# Pagina 2 ")
+    sl.markdown("# Pagina 2")
 
 
 
