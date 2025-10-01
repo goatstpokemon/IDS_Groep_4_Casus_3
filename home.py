948718ac6656631bfade50296055ec6c4915abe0
Luke Dixon <github@lukedixon.nl>
2025-10-01 12:02:42 +0200

    Add initial implementation of all_locations and thuispagina pages with Streamlit

---
+++ home.py
@@ -0,0 +1,31 @@
+
+import streamlit as sl
+import pandas as pd
+
+
+def main_page():
+    sl.markdown("# Homepagina 🦩")
+
+
+def page2():
+    sl.markdown("# All locations 📍")
+
+
+page_names_to_funcs = {
+    "Homepagina": main_page,
+    "Alle Locaties": page2
+
+}
+selected_page = sl.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
+page_names_to_funcs[selected_page]()
+
+
+
+
+
+
+
+
+
+
+
